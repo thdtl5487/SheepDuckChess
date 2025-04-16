dbd864311f6e5011fcc34ccaf6f2a0cc38fad210
thdtl5487 <thdtl5487@gmail.com>
2025-04-16 17:44:54 +0900

    redis 연결성공

--- backend/src/shared/config/redis.ts
+++ backend/src/shared/config/redis.ts
@@ -1,5 +1,19 @@
 import { createClient } from 'redis';
-const redis = createClient();
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config({
+    path: path.resolve(__dirname, '.env')
+});
+
+console.log(`포트 위치 : ${process.env.REDIS_PORT}`)
+
+const redis = createClient({
+    socket:{
+        host: process.env.REDIS_HOST,
+        port: Number(process.env.REDIS_PORT)
+    }
+});
 
 redis.connect().catch(console.error);
 export default redis;
