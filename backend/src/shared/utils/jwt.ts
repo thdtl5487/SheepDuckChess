2fa48f366bb14d9960bc797195a87bfbac7fb61c
thdtl5487 <thdtl5487@gmail.com>
2025-04-24 11:18:20 +0900

    jwt 삽입

---
+++ backend/src/shared/utils/jwt.ts
@@ -0,0 +1,13 @@
+import jwt, { SignOptions } from 'jsonwebtoken';
+import type { StringValue } from 'ms'; 
+
+const secret = process.env.JWT_SECRET || 'my-secret-token-key';
+
+export const signToken = (payload: object, expiresIn: StringValue = '1d') => {
+    const options: SignOptions = { expiresIn };
+    return jwt.sign(payload, secret, options);
+};
+
+export const verifyToken = (token: string): any => {
+    return jwt.verify(token, secret);
+};
